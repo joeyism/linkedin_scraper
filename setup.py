9ebaa1076dddd4238d13e3fd594eb22b6888fd8d
joeyism <sham.joey@gmail.com>
2017-10-27 17:04:07 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -10,4 +10,5 @@ setup(
         download_url = 'https://github.com/joeyism/linkedin_user_scraper/archive/0.0.1.tar.gz',
         keywords = ['linkedin', 'scraping', 'scraper'], 
         classifiers = [],
+        install_requires=['lxml', 'request', 'selenium'],
         )

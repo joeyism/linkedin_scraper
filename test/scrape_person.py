b712817856e9e262f6aa07879b6f42d8feeec466
joeyism <sham.joey@gmail.com>
2019-12-12 22:35:35 -0500

    fixed tests

--- test/scrape_person.py
+++ test/scrape_person.py
@@ -1,4 +1,4 @@
-from linkedin_user_scraper import scraper
+from linkedin_scraper import Person
 
-rick_fox = scraper.Person("https://www.linkedin.com/in/rifox?trk=pub-pbmap")
-iggy = scraper.Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5") 
+rick_fox = Person("https://www.linkedin.com/in/rifox?trk=pub-pbmap")
+iggy = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5") 

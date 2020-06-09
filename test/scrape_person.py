b5ac74f542f40d37b9c91c13d5b2c31e18845fff
AnirudraChoudhury <53123185+AnirudraChoudhury@users.noreply.github.com>
2020-06-09 16:58:31 +0530

    My Profile added

--- test/scrape_person.py
+++ test/scrape_person.py
@@ -1,4 +1,5 @@
 from linkedin_scraper import Person
 
 rick_fox = Person("https://www.linkedin.com/in/rifox?trk=pub-pbmap")
-iggy = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5") 
+iggy = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5")
+Anirudra = Person("https://in.linkedin.com/in/anirudra-choudhury-109635b1")

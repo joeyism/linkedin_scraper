877d7f8ae6f6b2e0559b96268c54f5acd6041baa
Bill Haofei Gong <x-magic@users.noreply.github.com>
2021-01-17 15:17:57 +1100

    Update person.py
    - Reformatted the file
    - Removed the scraper for not-logged-in user (unusable)
    - Scraping additional information about employment and education

--- linkedin_scraper/person.py
+++ linkedin_scraper/person.py
@@ -8,7 +8,6 @@ import os
 
 
 class Person(Scraper):
-
     __TOP_CARD = "pv-top-card"
 
     def reset(self):
@@ -19,20 +18,20 @@ class Person(Scraper):
         self.accomplishments.clear()
 
     def __init__(
-        self,
-        linkedin_url=None,
-        name=None,
-        about=[],
-        experiences=[],
-        educations=[],
-        interests=[],
-        accomplishments=[],
-        company=None,
-        job_title=None,
-        driver=None,
-        get=True,
-        scrape=True,
-        close_on_complete=True,
+            self,
+            linkedin_url=None,
+            name=None,
+            about=[],
+            experiences=[],
+            educations=[],
+            interests=[],
+            accomplishments=[],
+            company=None,
+            job_title=None,
+            driver=None,
+            get=True,
+            scrape=True,
+            close_on_complete=True,
     ):
         self.linkedin_url = linkedin_url
         self.name = name
@@ -87,7 +86,8 @@ class Person(Scraper):
         if self.is_signed_in():
             self.scrape_logged_in(close_on_complete=close_on_complete)
         else:
-            raise ReferenceError("You are not logged in anymore! It's very likely that you're blocked. The scraping will stop. Please restart the process when ready.")
+            raise ReferenceError(
+                "You are not logged in anymore! It's very likely that you're blocked. The scraping will stop. Please restart the process when ready.")
 
     def scrape_logged_in(self, close_on_complete=True):
         driver = self.driver
@@ -144,20 +144,20 @@ class Person(Scraper):
                     company = position.find_elements_by_tag_name("p")[1].text.strip()
                     times = str(
                         position.find_elements_by_tag_name("h4")[0]
-                        .find_elements_by_tag_name("span")[1]
-                        .text.strip()
+                            .find_elements_by_tag_name("span")[1]
+                            .text.strip()
                     )
                     from_date = " ".join(times.split(" ")[:2])
                     to_date = " ".join(times.split(" ")[3:])
                     duration = (
                         position.find_elements_by_tag_name("h4")[1]
-                        .find_elements_by_tag_name("span")[1]
-                        .text.strip()
+                            .find_elements_by_tag_name("span")[1]
+                            .text.strip()
                     )
                     location = (
                         position.find_elements_by_tag_name("h4")[2]
-                        .find_elements_by_tag_name("span")[1]
-                        .text.strip()
+                            .find_elements_by_tag_name("span")[1]
+                            .text.strip()
                     )
                 except:
                     company = None
@@ -192,8 +192,12 @@ class Person(Scraper):
             edu = None
         if edu:
             for school in edu.find_elements_by_class_name(
-                "pv-profile-section__list-item"
+                    "pv-profile-section__list-item"
             ):
+                university_uri = school.find_element_by_css_selector(
+                    "a[data-control-name=background_details_school]"
+                ).get_attribute("href").strip()
+
                 university = school.find_element_by_class_name(
                     "pv-entity__school-name"
                 ).text.strip()
@@ -201,22 +205,33 @@ class Person(Scraper):
                 try:
                     degree = (
                         school.find_element_by_class_name("pv-entity__degree-name")
-                        .find_elements_by_tag_name("span")[1]
-                        .text.strip()
+                            .find_elements_by_tag_name("span")[1]
+                            .text.strip()
                     )
+                except:
+                    degree = None
+                try:
+                    secondary_degree = (
+                        school.find_element_by_class_name("pv-entity__fos")
+                            .find_elements_by_tag_name("span")[1]
+                            .text.strip()
+                    )
+                except:
+                    secondary_degree = None
+                try:
                     times = (
                         school.find_element_by_class_name("pv-entity__dates")
-                        .find_elements_by_tag_name("span")[1]
-                        .text.strip()
+                            .find_elements_by_tag_name("span")[1]
+                            .text.strip()
                     )
                     from_date, to_date = (times.split(" ")[0], times.split(" ")[2])
                 except:
-                    degree = None
                     from_date, to_date = (None, None)
+
                 education = Education(
-                    from_date=from_date, to_date=to_date, degree=degree
+                    from_date=from_date, to_date=to_date, degree=str(degree) + " | " + str(secondary_degree)
                 )
-                education.institution_name = university
+                education.institution_name = str(university) + " | " + str(university_uri)
                 self.add_education(education)
 
         # get interest
@@ -234,7 +249,7 @@ class Person(Scraper):
                 "//*[@class='pv-profile-section pv-interests-section artdeco-container-card artdeco-card ember-view']"
             )
             for interestElement in interestContainer.find_elements_by_xpath(
-                "//*[@class='pv-interest-entity pv-profile-section__card-item ember-view']"
+                    "//*[@class='pv-interest-entity pv-profile-section__card-item ember-view']"
             ):
                 interest = Interest(
                     interestElement.find_element_by_tag_name("h3").text.strip()
@@ -257,11 +272,11 @@ class Person(Scraper):
                 "//*[@class='pv-profile-section pv-accomplishments-section artdeco-container-card artdeco-card ember-view']"
             )
             for block in acc.find_elements_by_xpath(
-                "//div[@class='pv-accomplishments-block__content break-words']"
+                    "//div[@class='pv-accomplishments-block__content break-words']"
             ):
                 category = block.find_element_by_tag_name("h3")
                 for title in block.find_element_by_tag_name(
-                    "ul"
+                        "ul"
                 ).find_elements_by_tag_name("li"):
                     accomplishment = Accomplishment(category.text, title.text)
                     self.add_accomplishment(accomplishment)
@@ -272,98 +287,7 @@ class Person(Scraper):
             driver.quit()
 
     def scrape_not_logged_in(self, close_on_complete=True, retry_limit=10):
-        driver = self.driver
-        retry_times = 0
-        while self.is_signed_in() and retry_times <= retry_limit:
-            page = driver.get(self.linkedin_url)
-            retry_times = retry_times + 1
-
-        # get name
-        self.name = driver.find_element_by_class_name(
-            "top-card-layout__title"
-        ).text.strip()
-
-        # get experience
-        try:
-            _ = WebDriverWait(driver, 3).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "experience"))
-            )
-            exp = driver.find_element_by_class_name("experience")
-        except:
-            exp = None
-
-        if exp is not None:
-            for position in exp.find_elements_by_class_name(
-                "experience-item__contents"
-            ):
-                position_title = position.find_element_by_class_name(
-                    "experience-item__title"
-                ).text.strip()
-                company = position.find_element_by_class_name(
-                    "experience-item__subtitle"
-                ).text.strip()
-
-                try:
-                    times = position.find_element_by_class_name(
-                        "experience-item__duration"
-                    )
-                    from_date = times.find_element_by_class_name(
-                        "date-range__start-date"
-                    ).text.strip()
-                    try:
-                        to_date = times.find_element_by_class_name(
-                            "date-range__end-date"
-                        ).text.strip()
-                    except:
-                        to_date = "Present"
-                    duration = position.find_element_by_class_name(
-                        "date-range__duration"
-                    ).text.strip()
-                    location = position.find_element_by_class_name(
-                        "experience-item__location"
-                    ).text.strip()
-                except:
-                    from_date, to_date, duration, location = (None, None, None, None)
-
-                experience = Experience(
-                    position_title=position_title,
-                    from_date=from_date,
-                    to_date=to_date,
-                    duration=duration,
-                    location=location,
-                )
-                experience.institution_name = company
-                self.add_experience(experience)
-        driver.execute_script(
-            "window.scrollTo(0, Math.ceil(document.body.scrollHeight/1.5));"
-        )
-
-        # get education
-        edu = driver.find_element_by_class_name("education__list")
-        for school in edu.find_elements_by_class_name("result-card"):
-            university = school.find_element_by_class_name(
-                "result-card__title"
-            ).text.strip()
-            degree = school.find_element_by_class_name(
-                "education__item--degree-info"
-            ).text.strip()
-            try:
-                times = school.find_element_by_class_name("date-range")
-                from_date = times.find_element_by_class_name(
-                    "date-range__start-date"
-                ).text.strip()
-                to_date = times.find_element_by_class_name(
-                    "date-range__end-date"
-                ).text.strip()
-            except:
-                from_date, to_date = (None, None)
-            education = Education(from_date=from_date, to_date=to_date, degree=degree)
-
-            education.institution_name = university
-            self.add_education(education)
-
-        if close_on_complete:
-            driver.close()
+        raise ReferenceError("You must login to continue!")
 
     @property
     def company(self):

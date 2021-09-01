22571a30c683f059bd77fedadf1305e0dec4d9e7
Alex Bujorianu <75983910+Alex-Bujorianu@users.noreply.github.com>
2021-09-01 12:36:39 +0200

    Added follow at random functionality
    This function randomly selects a company/user to follow, which is useful for spoofing human behaviour. Should only be called once the user has logged in.

--- linkedin_scraper/actions.py
+++ linkedin_scraper/actions.py
@@ -3,6 +3,7 @@ from . import constants as c
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+import random
 
 def __prompt_email_password():
   u = input("Email: ")
@@ -39,6 +40,17 @@ def login(driver, email=None, password=None, cookie = None, timeout=10):
     element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.ID, c.VERIFY_LOGIN_ID)))
   except: pass
   
+def pick_random_connection(driver):
+    "Choose a LinkedIn connection to follow, at random."
+    # Maximise the screen because the chat crap is blocking the button.
+    driver.maximize_window()
+    connections_list_member = driver.find_elements_by_class_name("feed-follows-module-recommendation.member")
+    connections_list_company = driver.find_elements_by_class_name("feed-follows-module-recommendation.company")
+    connections_list = connections_list_member + connections_list_company
+    random_number = random.randint(0, len(connections_list))
+    button = connections_list[random_number].find_element(By.TAG_NAME, "button")
+    button.click()
+    
 def _login_with_cookie(driver, cookie):
   driver.get("https://www.linkedin.com/login")
   driver.add_cookie({

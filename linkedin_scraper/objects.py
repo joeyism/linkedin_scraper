ed9104ac5d96bfe8a1c67bcadd6a0f43e0858947
Robert Arzola <robert@crehana.com>
2021-10-11 13:43:33 -0500

    feat: add dataclass in objects

--- linkedin_scraper/objects.py
+++ linkedin_scraper/objects.py
@@ -1,134 +1,60 @@
+from dataclasses import dataclass
+
+from selenium.webdriver import Chrome
+
 from . import constants as c
 
-class Contact(object):
-    name = None
-    occupation = None
-    url = None
 
-    def __init__(
-        self,
-        name=None,
-        occupation=None,
-        url=None
-    ):
-
-        self.name = name
-        self.occupation = occupation
-        self.url = url
-
-    def __repr__(self):
-        return "{name} ({occupation})".format(
-            name=self.name, occupation=self.occupation
-        )
+@dataclass
+class Contact:
+    name: str = None
+    occupation: str = None
+    url: str = None
 
 
-class Institution(object):
-    institution_name = None
-    website = None
-    industry = None
-    type = None
-    headquarters = None
-    company_size = None
-    founded = None
-
-    def __init__(
-        self,
-        name=None,
-        website=None,
-        industry=None,
-        type=None,
-        headquarters=None,
-        company_size=None,
-        founded=None,
-    ):
-        self.name = name
-        self.website = website
-        self.industry = industry
-        self.type = type
-        self.headquarters = headquarters
-        self.company_size = company_size
-        self.founded = founded
+@dataclass
+class Institution:
+    institution_name: str = None
+    website: str = None
+    industry: str = None
+    type: str = None
+    headquarters: str = None
+    company_size: int = None
+    founded: int = None
 
 
+@dataclass
 class Experience(Institution):
-    from_date = None
-    to_date = None
-    description = None
-    position_title = None
-    duration = None
-
-    def __init__(
-        self,
-        from_date=None,
-        to_date=None,
-        description=None,
-        position_title=None,
-        duration=None,
-        location=None,
-    ):
-        self.from_date = from_date
-        self.to_date = to_date
-        self.description = description
-        self.position_title = position_title
-        self.duration = duration
-        self.location = location
-
-    def __repr__(self):
-        return "{position_title} at {company} from {from_date} to {to_date} for {duration} based at {location}".format(
-            from_date=self.from_date,
-            to_date=self.to_date,
-            position_title=self.position_title,
-            company=self.institution_name,
-            duration=self.duration,
-            location=self.location,
-        )
+    from_date: str = None
+    to_date: str = None
+    description: str = None
+    position_title: str = None
+    duration: str = None
+    location: str = None
 
 
+@dataclass
 class Education(Institution):
-    from_date = None
-    to_date = None
-    description = None
-    degree = None
-
-    def __init__(self, from_date=None, to_date=None, description=None, degree=None):
-        self.from_date = from_date
-        self.to_date = to_date
-        self.description = description
-        self.degree = degree
-
-    def __repr__(self):
-        return "{degree} at {company} from {from_date} to {to_date}".format(
-            from_date=self.from_date,
-            to_date=self.to_date,
-            degree=self.degree,
-            company=self.institution_name,
-        )
+    from_date: str = None
+    to_date: str = None
+    description: str = None
+    degree: str = None
 
 
+@dataclass
 class Interest(Institution):
     title = None
 
-    def __init__(self, title=None):
-        self.title = title
 
-    def __repr__(self):
-        return self.title
-
-
+@dataclass
 class Accomplishment(Institution):
     category = None
     title = None
 
-    def __init__(self, category=None, title=None):
-        self.category = category
-        self.title = title
 
-    def __repr__(self):
-        return self.category + ": " + self.title
-
-
-class Scraper(object):
-    driver = None
+@dataclass
+class Scraper:
+    driver: Chrome = None
 
     def is_signed_in(self):
         try:

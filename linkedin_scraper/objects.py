594a56f27110596bce713afcb3b45694d34574a9
EDNA VIVIANA SEGURA ALVARADO <70909408+ednavivianasegura@users.noreply.github.com>
2022-09-07 17:37:14 +0200

    Changes due to Driver library update
    Changes due to Driver library update:
    Modification of the following functions:
    find_element_by_id(‘id’) by find_element(By.ID, ‘id’)
    find_element_by_name(‘name’) by find_element(By.NAME, ‘name’)
    find_element_by_xpath(‘xpath’) by 	find_element(By.XPATH, ‘xpath’)

--- linkedin_scraper/objects.py
+++ linkedin_scraper/objects.py
@@ -4,6 +4,8 @@ from selenium.webdriver import Chrome
 
 from . import constants as c
 
+from selenium.webdriver.common.by import By
+
 
 @dataclass
 class Contact:
@@ -58,7 +60,7 @@ class Scraper:
 
     def is_signed_in(self):
         try:
-            self.driver.find_element_by_id(c.VERIFY_LOGIN_ID)
+            self.driver.find_element(By.ID,c.VERIFY_LOGIN_ID)
             return True
         except:
             pass
@@ -74,7 +76,7 @@ class Scraper:
 
     def __find_element_by_xpath__(self, tag_name):
         try:
-            self.driver.find_element_by_xpath(tag_name)
+            self.driver.find_element(By.XPATH,tag_name)
             return True
         except:
             pass
@@ -82,7 +84,7 @@ class Scraper:
 
     def __find_enabled_element_by_xpath__(self, tag_name):
         try:
-            elem = self.driver.find_element_by_xpath(tag_name)
+            elem = self.driver.find_element(By.XPATH,tag_name)
             return elem.is_enabled()
         except:
             pass

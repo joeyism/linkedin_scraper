5c737d4bb2dfe65c5772fe7668592f5fbea9a1bf
Swagato Bag <swagatobag23@gmail.com>
2023-03-26 23:13:50 +0530

    Altered functions with updated ones

--- samples/scrape_person.py
+++ samples/scrape_person.py
@@ -1,9 +1,21 @@
-import os
-from linkedin_scraper import Person, actions
 from selenium import webdriver
-driver = webdriver.Chrome("./chromedriver")
+import time
+import sys
 
-email = os.getenv("LINKEDIN_USER")
-password = os.getenv("LINKEDIN_PASSWORD")
+# setting path
+sys.path.append('../linkedin_scraper')
+from linkedin_scraper import actions, Person
+
+driver = webdriver.Chrome("./chromedriver")
+time.sleep(2)
+driver.maximize_window()
+time.sleep(3)
+
+email = "info@bargad.ai"
+password = "Fizza123@"
 actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
-person = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5", driver=driver)
+time.sleep(2)
+
+person = Person("https://www.linkedin.com/in/swagatobag", driver=driver)
+
+print(vars(person))

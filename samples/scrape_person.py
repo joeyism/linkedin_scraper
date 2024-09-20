d62e4b821045b65aff982c73288e159cef7ae984
Nguyen Quoc Viet <nguyenqviet98@gmail.com>
2024-09-20 14:48:28 +0800

    Revert "create input loop and remove unneeded flow"
    This reverts commit d942d69b430a7951a7e31f61b49812343a476c9a.
    
    Revert "create loop program, log when error, and fix scraper"
    
    This reverts commit 31612bf9f9080536060d929b4ca4fe1e3397f66c.
    
    Revert "Update README.md"
    
    This reverts commit 9ec8d1017c860badf1a87d303a6063d0ec20f705.
    
    Revert "Update README.md"
    
    This reverts commit 896c5f3e575654c5a566aa404e1f04af0f128ca2.

--- samples/scrape_person.py
+++ samples/scrape_person.py
@@ -1,36 +1,9 @@
 import os
-from linkedin_scraper import Person, actions, Company
+from linkedin_scraper import Person, actions
 from selenium import webdriver
-
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("./chromedriver")
 
 email = os.getenv("LINKEDIN_USER")
 password = os.getenv("LINKEDIN_PASSWORD")
-actions.login(driver, email, password)  # if email and password isnt given, it'll prompt in terminal
-user_input = []
-urls = []
-while True:
-    user_input = input("Enter a comma-separated list of linkedin urls: ")
-    if user_input == "exit":
-        break
-    urls = user_input.split(",")
-    results = []
-    for url in urls:
-        print(f'scraping {url}')
-        person = Person(url,  driver=driver, close_on_complete=False)
-        company = Company(person.experiences[0].linkedin_url, get_employees=False, driver=driver, close_on_complete=False)
-        results.append((person, company))
-
-    print('RESULTS:')
-    print('name,location,exp_title,exp_company,exp_linkedin,company_industry,company_website,company_size')
-    for person, company in results:
-        experience = person.experiences[0]
-        print(f'"{person.name}", '
-              f'"{person.location}", '
-              f'"{experience.position_title}", '
-              f'"{experience.institution_name}", '
-              f'"{experience.linkedin_url}", '
-              f'"{company.industry}", '
-              f'"{company.website}", '
-              f'"{company.company_size}", '
-              )
+actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
+person = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5", driver=driver)

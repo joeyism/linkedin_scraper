ab09e8998ab92a5c6f61181218dc7b7331f9bc65
joeyism <sham.joey@gmail.com>
2024-12-13 11:54:52 +0100

    Updated version

--- linkedin_scraper/__init__.py
+++ linkedin_scraper/__init__.py
@@ -5,7 +5,7 @@ from .company import Company
 from .jobs import Job
 from .job_search import JobSearch
 
-__version__ = "2.11.4"
+__version__ = "2.11.5"
 
 import glob
 modules = glob.glob(dirname(__file__)+"/*.py")
